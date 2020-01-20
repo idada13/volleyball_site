a593f307c938d0a67e5e8bbbd49e6d5a02849df9
Aga <agnieszka@Jamess-MBP.home>
2020-01-20 20:06:48 +0000

    Aga's first template changes

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def home_view(request, *args, **kwargs):
-	return render(request, "index.html", {})
+	list_of_dates = ["11/12", "01/03", "03/06"]
+	return render(request, "index.html", {"dates": list_of_dates})
     #return HttpResponse("You're at the homepage index.")
